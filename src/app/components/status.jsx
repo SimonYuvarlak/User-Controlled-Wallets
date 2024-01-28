07e790c7b89dfe6ad0983612800ba4f7ad4b7568
Simon Arda Yuvarlak <ardayuvarlak@gmail.com>
2024-01-28 13:32:11 -0500

    status bug fix

--- src/app/components/status.jsx
+++ src/app/components/status.jsx
@@ -4,8 +4,7 @@ const Status = ({ status, flag }) => {
   return (
     <div
       className={
-        "rounded-lg w-[%80] p-2 border-2 mx-8 flex justify-center" +
-        (flag && status === "201")
+        flag === true && status === 201
           ? "rounded-lg w-[%80] p-2 border-2 mx-8 flex justify-center bg-green-700"
           : "rounded-lg w-[%80] p-2 border-2 mx-8 flex justify-center bg-red-500"
       }
